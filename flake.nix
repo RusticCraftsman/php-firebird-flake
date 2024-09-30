556c61c6977e7afed4715b58cd2b53496c631903
Elkellympia <Elkellympia@protonmail.com>
2024-09-30 00:54:50 -0300

    3

--- flake.nix
+++ flake.nix
@@ -3,13 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    overlay = final: prev: {
-      phpExtensions = prev.phpExtensions // {
-        firebird = final.callPackage ./firebird.nix { };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+        php-with-firebird = pkgs.php.withExtensions ({ all, ... }: [ all.firebird ]);
+      in
+      {
+        packages.default = php-with-firebird;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            php-with-firebird
+          ];
+        };
+      }
+    ) // {
+      overlay = final: prev: {
+        phpExtensions = prev.phpExtensions // {
+          firebird = final.callPackage ./firebird.nix { };
+        };
       };
     };
-  };
 }
